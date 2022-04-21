ea8a7b92e3c47c1e481963e66b502ab24593d441
Asadullah Khan <asadullahk15@gmail.com>
2022-04-21 14:01:39 +0500

    Register Controller

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -1,7 +1,51 @@
-exports.login = async (req, res) => {
-  res.send("Login");
-};
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
 exports.register = async (req, res) => {
-  res.send("Register");
+  try {
+    const { username, mail, password } = req.body;
+
+    // check existing user
+    const existingUser = await User.exists({ mail });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // encrypt password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // create user document and save in database
+
+    const user = new User({
+      username,
+      mail,
+      password: encryptedPassword,
+    });
+
+    await user.save();
+
+    // create JWT token
+    const token = "JWT TOKEN";
+
+    res.status(201).json({
+      message: "User created successfully",
+      userDetails: {
+        username,
+        mail,
+        token,
+      },
+    });
+  } catch (err) {
+    res.status(500).send("Error occurred . Please Try Again!");
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
