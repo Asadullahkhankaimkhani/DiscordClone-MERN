8198d4c33183c99d4d1c2d356b083333e75386d1
Asadullah Khan <asadullahk15@gmail.com>
2022-04-22 10:57:04 +0500

    Routes with React Route 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+// Pages
+import Login from "./pages/AuthPages/Login";
+import Register from "./pages/AuthPages/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
