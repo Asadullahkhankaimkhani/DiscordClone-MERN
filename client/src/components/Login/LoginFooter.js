b78753faf0761510010574937acf64ab2596022b
unknown <asadullahk15@gmail.com>
2022-04-22 21:21:56 +0500

    Redirect Component

--- client/src/components/Login/LoginFooter.js
+++ client/src/components/Login/LoginFooter.js
@@ -1,16 +1,32 @@
 import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import { useHistory } from "react-router-dom";
 
 const LoginFooter = ({ handleLogin, isFormValid }) => {
+  const history = useHistory();
+
+  const handlePushToRegister = () => {
+    history.push("/register");
+  };
+
   return (
-    <div>
-      <CustomPrimaryButton
-        label="Login"
+    <>
+      <div>
+        <CustomPrimaryButton
+          label="Login"
+          additionalStyles={{ marginTop: "30px" }}
+          onClick={handleLogin}
+          disabled={!isFormValid}
+        />
+      </div>
+      <RedirectInfo
+        text="Need an account?  "
+        redirectText="Create an account"
+        redirectHandler={handlePushToRegister}
         additionalStyles={{ marginTop: "30px" }}
-        onClick={handleLogin}
-        disabled={!isFormValid}
       />
-    </div>
+    </>
   );
 };
 
