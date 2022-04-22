53dabc28f1677629a497a53f7b1d6be00a5a1add
Asadullah Khan <asadullahk15@gmail.com>
2022-04-22 13:09:46 +0500

    Custom Input Components

--- client/src/pages/AuthPages/Login.js
+++ client/src/pages/AuthPages/Login.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginHeader from "../../components/Login/LoginHeader";
+import LoginInputs from "../../components/Login/LoginInputs";
 
 function Login() {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <AuthBox>
       <LoginHeader />
+      <LoginInputs
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+      />
     </AuthBox>
   );
 }
