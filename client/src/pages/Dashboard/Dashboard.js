d1189faceb3fdfb091722aeeaf128e6865530b76
unknown <asadullahk15@gmail.com>
2022-04-24 21:08:32 +0500

    Creating Dashboard Structure

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { styled } from "@mui/system";
+
+// Components
+import SideBar from "../../components/Sidebar/SideBar";
+import FriendsSideBar from "../../components/FriendsSideBar/FriendsSideBar";
+import Messenger from "../../components/Messenger/Messenger";
+import AppBar from "../../components/AppBar/AppBar";
+
+const Wrapper = styled("div")({
+  display: "flex",
+  width: "100%",
+  height: "100vh",
+});
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Wrapper>
+      <SideBar />
+      <FriendsSideBar />
+      <Messenger />
+      <AppBar />
+    </Wrapper>
+  );
 }
 
 export default Dashboard;
