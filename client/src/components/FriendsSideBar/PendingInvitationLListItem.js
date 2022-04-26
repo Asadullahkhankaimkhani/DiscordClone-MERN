086c4a53695befe5767a82b1f09eedc9a726e585
Asadullah Khan <asadullahk15@gmail.com>
2022-04-26 14:45:18 +0500

    Creating Dummy Invitation List

---
+++ client/src/components/FriendsSideBar/PendingInvitationLListItem.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Tooltip, Typography, Box } from "@mui/material";
+
+import Avatar from "../../shared/components/Avatar";
+
+const PendingInvitationLListItem = ({
+  id,
+  username,
+  mail,
+  acceptFriendInvitation = () => {},
+  rejectFriendInvitation = () => {},
+}) => {
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const handleAcceptInvitation = () => {
+    acceptFriendInvitation({ id });
+    setButtonDisabled(true);
+  };
+
+  const handleRejectInvitation = () => {
+    rejectFriendInvitation({ id });
+    setButtonDisabled(true);
+  };
+
+  return (
+    <Tooltip title={mail}>
+      <div style={{ width: "100%" }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "42px",
+            marginTop: "10px",
+            display: "flex",
+            alginItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Avatar username={username} />
+          <Typography
+            sx={{
+              marginLeft: "7px",
+              fontWeight: 700,
+              color: "#8e9297",
+              flexGrow: 1,
+            }}
+            variant="subtitle1"
+          >
+            {username}
+          </Typography>
+        </Box>
+      </div>
+    </Tooltip>
+  );
+};
+
+export default PendingInvitationLListItem;
--- client/src/components/FriendsSideBar/PendingInvitationsList.js
+++ client/src/components/FriendsSideBar/PendingInvitationsList.js
@@ -1,5 +1,23 @@
 import React from "react";
 import { styled } from "@mui/system";
+import PendInvitationListItem from "./PendingInvitationLListItem";
+
+const Invitations = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Mark",
+      mail: "dummy@ad.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "Mark",
+      mail: "Demo@gmail.com",
+    },
+  },
+];
 
 const MainContainer = styled("div")({
   width: "100%",
@@ -11,7 +29,20 @@ const MainContainer = styled("div")({
 });
 
 const PendingInvitationsList = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      {Invitations.map((invitation) => {
+        return (
+          <PendInvitationListItem
+            key={invitation._id}
+            id={invitation._id}
+            username={invitation.senderId.username}
+            mail={invitation.senderId.mail}
+          />
+        );
+      })}
+    </MainContainer>
+  );
 };
 
 export default PendingInvitationsList;
